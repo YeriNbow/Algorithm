bcc6b2061f7f0dfa090e5bf104316904707ce093
Sang-Hyeong Jin <jwjln3126@naver.com>
2020-10-07 20:24:52 +0900

    Update dfs_bfs.py

--- useful/dfs_bfs.py
+++ useful/dfs_bfs.py
@@ -3,15 +3,12 @@ import collections
 n, m, v = map(int, input().split())
 
 graph = collections.defaultdict(list)
+
 for i in range(m):
     N, M = map(int, input().split())
     graph[N].append(M)
     graph[M].append(N)
 
-
-# dfs, bfs의 수도코드대로 구현
-# dfs는 재귀가 가능하나 bfs는 재귀가 불가능
-# dfs는 스택, bfs는 큐를 이용해 구현
 
 def dfs(current, route):
     route.append(current)
