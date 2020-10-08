c4a0e4eecc83fcd78f5fa02ce83e0606bb2c309f
YeriNbow <jwjln3126@naver.com>
2020-10-08 21:28:03 +0900

    Create 2812.py

---
+++ problems/2812.py
@@ -0,0 +1,16 @@
+n, k = map(int, input().split())
+num = list(input())
+
+stack = []
+tmp = k
+
+for i in range(n):
+    while stack and tmp > 0:
+        if stack[-1] < num[i]:
+            stack.pop()
+            tmp -= 1
+        else:
+            break
+    stack.append(num[i])
+
+print(''.join(stack[:n-k]))
