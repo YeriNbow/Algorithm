150a40bd72e163e875b77b23ed4e8380b1123bae
YeriNbow <jwjln3126@naver.com>
2020-10-25 10:53:23 +0900

    Update binary_search_function.py

--- useful/Data Structure/binary_search_function.py
+++ useful/Data Structure/binary_search_function.py
@@ -2,12 +2,12 @@ def binary_search(num_list, x, left, right):
     if left <= right:
         mid = left + (right - left) // 2
 
-        if num_list[mid] == x:
-            return mid
+        if num_list[mid] < x:
+            return binary_search(num_list, x, mid+1, right)
         elif num_list[mid] > x:
-            return binary_search(num_list, x, left, mid+1)
+            return binary_search(num_list, x, left, mid-1)
         else:
-            return binary_search(num_list, x, mid, right)
+            return mid
     else:
         return None
 
@@ -16,5 +16,5 @@ if __name__ == '__main__':
     num = [50, 30, 24, 5, 28, 45, 98, 52, 60]
     num.sort()
 
-    idx = binary_search(num, 30, 0, len(num))
+    idx = binary_search(num, 5, 0, len(num))
     print(num[idx])
